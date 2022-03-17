d1b8f854dabee8df9720af1cbf3558c2863431ae
Mya-Mya <interleukin413@gmail.com>
2022-03-17 20:30:35 +0900

    First Commit

--- App.js
+++ App.js
@@ -1,20 +1,12 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from "react";
+import { NativeBaseProvider } from "native-base";
+import { RecoilRoot } from "recoil";
+import RooteView from "./src/RooteView";
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
-}
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+export default () => (
+  <NativeBaseProvider>
+    <RecoilRoot>
+      <RooteView />
+    </RecoilRoot>
+  </NativeBaseProvider>
+);
---
+++ src/RooteView.js
@@ -0,0 +1,7 @@
+import React from "react";
+import { View, Text } from "native-base";
+export default () => (
+  <View>
+    <Text>RooteView</Text>
+  </View>
+);
