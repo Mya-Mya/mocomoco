fba33283840e1034784f779fc4c0e490cbd7aa0e
Mya-Mya <interleukin413@gmail.com>
2022-03-20 14:12:54 +0900

    Add GitHub link

--- src/scenes/AboutScene.js
+++ src/scenes/AboutScene.js
@@ -10,14 +10,16 @@ import {
 } from "native-base";
 import { useRecoilCallback } from "recoil";
 import sceneState, { SCENE_NAMES } from "../states/sceneState";
+import * as WebBrowser from "expo-web-browser";
 
 export const useAboutActions = () => {
   const onReturnPress = useRecoilCallback(({ set }) => () => {
     console.log("About.onReturnPress");
     set(sceneState, SCENE_NAMES.HOME);
   });
-  const onOpenRepositoryPress = useRecoilCallback(() => () => {
+  const onOpenRepositoryPress = useRecoilCallback(() => async () => {
     console.log("About.onOpenRepositoryPress");
+    await WebBrowser.openBrowserAsync("https://github.com/Mya-Mya/mocomoco");
   });
   return { onReturnPress, onOpenRepositoryPress };
 };
@@ -31,12 +33,10 @@ export const AboutView = () => {
           <Heading>Mocomoco</Heading>
           <Text>HTMLなどのソースコードを書いて試せるアプリです。</Text>
           <Text textAlign="right">Developed by やいも 2022-03</Text>
-          <HStack space="xs">
-            <Button onPress={actions.onReturnPress}>戻る</Button>
-            <Button variant="outline" onPress={actions.onOpenRepositoryPress}>
-              リポジトリ
-            </Button>
-          </HStack>
+          <Button variant="outline" onPress={actions.onOpenRepositoryPress}>
+            GitHub
+          </Button>
+          <Button onPress={actions.onReturnPress}>戻る</Button>
         </VStack>
       </Center>
     </View>
