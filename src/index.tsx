ef992e0c5d4c08346b0c8f03160d1a187e43aef8
Mya-Mya <interleukin413@gmail.com>
2022-03-26 19:28:22 +0900

    Create index.tsx

---
+++ src/index.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import { RecoilRoot } from "recoil";
+
+ReactDOM.render(
+  <RecoilRoot>
+    <h1>Hello, world!</h1>
+  </RecoilRoot>,
+  document.getElementById("root")
+);
