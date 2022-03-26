b7bcb7c8880fcdad526f3fa9073c250cba6bfa47
Mya-Mya <interleukin413@gmail.com>
2022-03-26 20:30:41 +0900

    Update Dialog.stories.tsx

--- src/components/Dialog.stories.tsx
+++ src/components/Dialog.stories.tsx
@@ -1,27 +1,65 @@
 import React from "react";
 import Dialog from "./Dialog";
 import { ComponentStory, ComponentMeta } from "@storybook/react";
+import XSSWarning from "./XSSWarning";
 export default {
   title: "Dialog",
   component: Dialog,
 } as ComponentMeta<typeof Dialog>;
 
-export const Default: ComponentStory<typeof Dialog> = (args) => (
+export const WithXSSWarning: ComponentStory<typeof Dialog> = (args) => (
   <Dialog {...args} />
 );
-Default.args = {
+WithXSSWarning.args = {
   isOpen: true,
   setIsOpen: (_isOpen: boolean) => {
     alert("setIsOpen");
   },
-  title: "title",
-  message: "message",
+  title: "重要なお知らせ",
+  children: <XSSWarning />,
   selections: [
+    {
+      label: "Detail",
+      closeOnPress: true,
+      onPress: () => {
+        alert("Detail Press");
+      },
+      variant: "outlined",
+    },
     {
       label: "Close",
       closeOnPress: true,
       onPress: () => {
         alert("Close Press");
+      },
+      variant: "contained",
+    },
+  ],
+};
+export const Normal: ComponentStory<typeof Dialog> = (args) => (
+  <Dialog {...args} />
+);
+Normal.args = {
+  isOpen: true,
+  setIsOpen: (isOpen: boolean) => {
+    console.log("setIsOpen", isOpen);
+  },
+  title: "ファイルの削除",
+  message: "削除しますか？:index.html",
+  selections: [
+    {
+      label: "キャンセル",
+      closeOnPress: true,
+      onPress: () => {
+        alert("キャンセル");
+      },
+      variant: "outlined",
+    },
+    {
+      label: "削除する",
+      closeOnPress: true,
+      onPress: () => {
+        alert("削除する");
       },
       variant: "contained",
     },
