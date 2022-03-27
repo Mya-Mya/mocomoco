4dc5531e586b8b52748c47fb1b0e824fa46631e6
Mya-Mya <interleukin413@gmail.com>
2022-03-27 14:46:01 +0900

    render RootView

--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,11 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import { RecoilRoot } from "recoil";
+import RootView from "./RootView";
 
 ReactDOM.render(
   <RecoilRoot>
-    <h1>Hello, world!</h1>
+    <RootView />
   </RecoilRoot>,
   document.getElementById("root")
 );
