4475f21b07993482c7235b4ecd9e04bf57e3632b
Mya-Mya <interleukin413@gmail.com>
2022-03-27 14:45:27 +0900

    RooteView nr js->react ts

---
+++ src/RootView.tsx
@@ -0,0 +1,23 @@
+import React from "react";
+import { useRecoilValue } from "recoil";
+import { AboutView } from "./scenes/AboutScene";
+import { CreateFileView } from "./scenes/CreateFileScene";
+import { FileEditorView } from "./scenes/FileEditorScene";
+import { HomeView } from "./scenes/HomeScene";
+import sceneState, { SCENE_NAMES } from "./states/sceneState";
+//import { Routes, Route, BrowserRouter } from "react-router-dom";
+
+export default () => {
+  const scene = useRecoilValue(sceneState);
+  switch (scene) {
+    case SCENE_NAMES.HOME:
+      return <HomeView />;
+    case SCENE_NAMES.ABOUT:
+      return <AboutView />;
+    case SCENE_NAMES.FILE_EDITOR:
+      return <FileEditorView />;
+    case SCENE_NAMES.CREATE_FILE:
+      return <CreateFileView />;
+  }
+  return <div>{scene}</div>;
+};
